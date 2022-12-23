fc42e12dcc5283930c1a52ee0557452cb8fabd71
ahmedelgohary305 <112413930+ahmedelgohary305@users.noreply.github.com>
2022-12-23 20:54:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isprime=true;
+            int n1, n2;
+
+            Console.WriteLine("enter n1:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter n2:");
+            n2 = int.Parse(Console.ReadLine());
+            
+            for (int x = n1; x <= n2; x++)
+            {
+                for (int y = 2; y <= n2;y++)
+                {
+                    if (x!=y && x % y == 0)
+                    {
+                        isprime = false;
+                        break;
+                    }
+
+                }
+                if(isprime )
+                {
+                    Console.WriteLine(x);
+                }
+                isprime = true;
+            }
+           
+
+        }
+    }
+}
